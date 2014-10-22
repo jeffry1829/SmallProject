9c3a2ee4aa24715c788687a0e97cf1316d3c686b
jeffry1829 <jeffry1829@gmail.com>
2014-10-23 02:07:02 +0800

    v2_re

--- wallpaper/Main_Old.java
+++ wallpaper/Main_Old.java
@@ -3,7 +3,7 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.imageio.*;
 import javax.swing.*;
-class Wallpaper implements ActionListener{
+class Wallpaper_2 implements ActionListener{
 	Button[] button;
 	static int Timer;
 	static JFrame f;
@@ -17,7 +17,7 @@ class Wallpaper implements ActionListener{
 	//
 public static void Show()
 	 {
-		 Wallpaper wall=new Wallpaper();
+		 Wallpaper_2 wall=new Wallpaper_2();
 		 f = new JFrame("請選擇"); 		
 		 
 		 f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
@@ -45,11 +45,14 @@ public void RUN() throws Exception
 	FileWriter fw = new FileWriter("./"+random+"wallpaper.xml",true);
         File f = new File("./"+random+"wallpaper.xml");
 	BufferedWriter bw=new BufferedWriter(fw);
+	bw.write("<background><starttime><year>2014</year><month>10</month><day>6</day><hour>00</hour><minute>00</minute><second>00</second></starttime>");
         if(!f.exists()){f.createNewFile();}    
         for(int i=0;i<Timer;i++)
-{
-	bw.write("\r\n"+Filename[i]);
+{if(i!=Timer-1){
+	bw.write("<static><duration>30.0</duration><file>"+Filename[i]+"</file></static><transition><duration>5.0</duration><from>"+Filename[i]+"</from><to>"+Filename[i+1]+"</to></transition>");
+}else{"<static><duration>30.0</duration><file>"+Filename[i]+"</file></static>"}
 }
+bw.write("</background>");
 bw.close();
 }
 public void Return(int Number)
